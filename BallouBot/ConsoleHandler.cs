0f67be4c4445e59cee3aab4742f5b8db88b23bbc
Ted Crocker <edward.b.crocker@gmail.com>
2015-09-20 11:47:34 -0400

    Stuff for handling console input/output.

---
+++ BallouBot/ConsoleHandler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading;
+
+namespace BallouBot
+{
+	public class ConsoleHandler : IChatParser
+	{
+		private ICommandQueue _commandQueue;
+		private bool _running = true;
+
+		public ConsoleHandler(ICommandQueue commandQueue)
+		{
+			_commandQueue = commandQueue;
+			Thread thread = new Thread(ReadConsoleInput);
+			thread.IsBackground = true;
+			thread.Start();
+		}
+
+		public void ReadConsoleInput()
+		{
+			while (_running)
+			{
+				var command = Console.ReadLine();
+				_commandQueue.EnqueueCommand(command);
+			}
+		}
+
+		public void ReceiveMessage(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
