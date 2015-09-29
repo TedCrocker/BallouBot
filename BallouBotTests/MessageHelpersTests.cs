63b6fdcd5e1bd7232f041220830239bebd0a4ca1
Ted Crocker <edward.b.crocker@gmail.com>
2015-09-29 11:47:29 -0400

    Some more work on tests.

---
+++ BallouBotTests/MessageHelpersTests.cs
@@ -0,0 +1,19 @@
+﻿using BallouBot;
+using BallouBot.Core;
+using Xunit;
+
+namespace BallouBotTests
+{
+	public class MessageHelpersTests
+	{
+		[Fact]
+		public void PrivateMessageFormattedCorrectly()
+		{
+			var message = MessageParser.ParseIrcMessage(":ballouthebear!ballouthebear@ballouthebear.tmi.twitch.tv PRIVMSG #ballouthebear :books");
+			var outgoingMessage = "Hello to you!";
+
+			var formattedMessage = MessageHelpers.PrivateMessage(message, outgoingMessage);
+			Assert.Equal(formattedMessage, "PRIVMSG #ballouthebear :Hello to you!");
+		}
+	}
+}
--- BallouBotTests/MessageParserTests.cs
+++ BallouBotTests/MessageParserTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.Serialization;
-using BallouBot;
 using BallouBot.Core;
 using Xunit;
 
