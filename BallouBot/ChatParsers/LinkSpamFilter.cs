50570bc93826264c377cccaa714c78e0d16cf4be
Ted Crocker <edward.b.crocker@gmail.com>
2015-10-06 16:00:57 -0400

    Mods can permit users to post links.

--- BallouBot/ChatParsers/LinkSpamFilter.cs
+++ BallouBot/ChatParsers/LinkSpamFilter.cs
@@ -8,9 +8,9 @@ namespace BallouBot.ChatParsers
 {
 	public class LinkSpamFilter : IChatParser
 	{
-		private IDataSource _dataSource;
-		private ICommandQueue _commandQueue;
-		private static Regex _webUrlRegex = new Regex(@"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))");
+		private readonly IDataSource _dataSource;
+		private readonly ICommandQueue _commandQueue;
+		private static readonly Regex WebUrlRegex = new Regex(@"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))");
 
 		public LinkSpamFilter(ICommandQueue commandQueue, IDataSource dataSource)
 		{
@@ -20,13 +20,45 @@ namespace BallouBot.ChatParsers
 
 		public async Task ReceiveMessage(Message message)
 		{
-			if (message.Command == Constants.PrivateMessageCommand && _webUrlRegex.IsMatch(message.Suffix))
+			if (message.Command == Constants.PrivateMessageCommand)
 			{
-				var userIsMod = await IsUserMod(message.User, message.Channel);
-				if (!userIsMod)
+				if (WebUrlRegex.IsMatch(message.Suffix))
 				{
-					_commandQueue.EnqueueCommand(MessageHelpers.PrivateMessage(message, ".timeout " + message.User + " 1"));
+					await HandleMessageWithLink(message);
 				}
+				if (message.Suffix.Contains("!permit"))
+				{
+					await HandlePermitUserCommand(message);
+				}
+			}
+		}
+
+		private async Task HandlePermitUserCommand(Message message)
+		{
+			var userIsMod = await IsUserMod(message.User, message.Channel);
+			if (userIsMod)
+			{
+				var suffixWords = message.Suffix.Split(' ');
+				if (suffixWords.Length >= 2)
+				{
+					var userName = suffixWords[1];
+					var user = await _dataSource.Repository<User>().Get(userName);
+					if (user != null)
+					{
+						user.Data[message.Channel + "-hasLinkPermission"] = true;
+						await _dataSource.Repository<User>().Update(user.Id, user);
+						_commandQueue.EnqueueCommand(MessageHelpers.PrivateMessage(message, "User " + userName + " has been permitted to post a link."));
+					}
+				}
+			}
+		}
+
+		private async Task HandleMessageWithLink(Message message)
+		{
+			var userIsModOrPermitted = await IsUserMod(message.User, message.Channel) || await DoesUserHavePermission(message.User, message.Channel);
+			if (!userIsModOrPermitted)
+			{
+				_commandQueue.EnqueueCommand(MessageHelpers.PrivateMessage(message, ".timeout " + message.User + " 1"), QueuePriority.High);
 			}
 		}
 
@@ -40,6 +72,21 @@ namespace BallouBot.ChatParsers
 				isMod = true;
 			}
 			return isMod;
+		}
+
+		private async Task<bool> DoesUserHavePermission(string userId, string channel)
+		{
+			var isPermitted = false;
+			var user = await _dataSource.Repository<User>().Get(userId);
+
+			if (user.Data.ContainsKey(channel + "-hasLinkPermission"))
+			{
+				isPermitted = true;
+				user.Data.Remove(channel + "-hasLinkPermission");
+				await _dataSource.Repository<User>().Get(userId);
+			}
+
+			return isPermitted;
 		}
 	}
 }
--- BallouBotTests/LinkSpamHandlerTests.cs
+++ BallouBotTests/LinkSpamHandlerTests.cs
@@ -50,5 +50,51 @@ namespace BallouBotTests
 			var command = commandQueue.DequeueCommand();
 			Assert.Equal(command, "");
 		}
+
+		[Fact]
+		public async void AdminCanGratPermissionToUserForLinks()
+		{
+			var commandQueue = new MockCommandQueue();
+			var dataSource = new MockDataSource();
+			var repo = dataSource.Repository<User>() as MockRepository<User>;
+			repo.ObjectCache.Add("ballouthebear", new User()
+			{
+				Channels = new Dictionary<string, UserChannel>()
+				{
+					{
+						"#ballouthebear", new UserChannel()
+						{
+							IsModerator = true
+						}
+					}
+				},
+				Data = new Dictionary<string, object>()
+			});
+			repo.ObjectCache.Add("taylormade86", new User()
+			{
+				Channels = new Dictionary<string, UserChannel>()
+				{
+					{
+						"#ballouthebear", new UserChannel()
+						{
+							IsModerator = false
+						}
+					}
+				},
+				Data = new Dictionary<string, object>()
+			});
+
+			var linkSpamHandler = new LinkSpamFilter(commandQueue, dataSource);
+			var adminMessage = MessageParser.ParseIrcMessage("@color=#FF0000;display-name=BallouTheBear;emotes=;subscriber=0;turbo=0;user-id=30514348;user-type= :ballouthebear!ballouthebear@ballouthebear.tmi.twitch.tv PRIVMSG #ballouthebear :!permit taylormade86");
+			await linkSpamHandler.ReceiveMessage(adminMessage);
+			var command = commandQueue.DequeueCommand();
+			Assert.Equal(command, "PRIVMSG #ballouthebear :User taylormade86 has been permitted to post a link.");
+
+			var userMessage = MessageParser.ParseIrcMessage("@color=#FF0000;display-name=Taylormade86;emotes=;subscriber=0;turbo=0;user-id=30514348;user-type= :taylormade86!taylormade86@taylormade86.tmi.twitch.tv PRIVMSG #ballouthebear :Check it out! www.google.com");
+			await linkSpamHandler.ReceiveMessage(userMessage);
+
+			command = commandQueue.DequeueCommand();
+			Assert.Equal(command, "");
+		}
 	}
 }
